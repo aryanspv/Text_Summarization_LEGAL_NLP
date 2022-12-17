c5f1e840846fcde1cb198e0289ba85b92403c76d
Aryan Sharma <64528511+aryanspv@users.noreply.github.com>
2022-12-18 04:01:21 +0530

    Update main.py

--- main.py
+++ main.py
@@ -101,25 +101,25 @@ with header:
     st.title('Text Summarization using NLTK')
 
 with body:
-    st.header('Extractive Summarization')
-    rawtext = st.text_area('Enter Text Here')
+    #st.header('Extractive Summarization')
+    #rawtext = st.text_area('Enter Text Here')
 
-    sample_col, upload_col = st.beta_columns(2)
-    sample_col.header('Or select a sample file from below')
-    sample = sample_col.selectbox('Or select a sample file',
-                                  ('kalam_speech.txt', 'Stocks_ FRI_ JUN _8.txt', 'microsoft.txt', 'None'), index=3)
-    if sample != 'None':
-        file = open(sample, "r", encoding= 'cp1252')
+    #sample_col, upload_col = st.beta_columns(2)
+    #sample_col.header('Or select a sample file from below')
+    #sample = sample_col.selectbox('Or select a sample file',
+                                  #('kalam_speech.txt', 'Stocks_ FRI_ JUN _8.txt', 'microsoft.txt', 'None'), index=3)
+    #if sample != 'None':
+      #  file = open(sample, "r", encoding= 'cp1252')
         #st.write(file)
-        rawtext = file.read()
+       # rawtext = file.read()
 
-    upload_col.header('Or upload text file here')
+    upload_col.header('Upload text file here')
     uploaded_file = upload_col.file_uploader(
         'Choose your .txt file', type="txt")
     if uploaded_file is not None:
         rawtext = str(uploaded_file.read(), 'cp1252')
-
-    no_of_lines = st.slider("Select number of lines in summary", 1, 5, 3)
+        
+    no_of_lines = st.slider("Select number of lines in summary", 3, 5, 7)
     if st.button('Get Summary'):
         with summary_container:
             if rawtext == "":
